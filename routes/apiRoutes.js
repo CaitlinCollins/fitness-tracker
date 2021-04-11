b8c163361118992b6674c8b3aa42c184dc139d15
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-04-10 19:45:22 -0500

    Setup html routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { Exercise } = require('../models/Exercise');
+
+
+// Add to Recent Workout
+
+
+// Add to new Workout
+
+
+// Get Stats
+
+// Post to Stats
+
