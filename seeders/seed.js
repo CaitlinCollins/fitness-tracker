978bbca54341372fc9ce092db99ff1fea15c3d6c
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-04-12 18:35:49 -0500

    Resolved issue with seeding db

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,11 @@
 let mongoose = require("mongoose");
-let db = require("../models/Exercise");
+let Workout = require("../models/Workout");
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
 
 let workoutSeed = [
   {
@@ -124,8 +125,8 @@ let workoutSeed = [
   }
 ];
 
-db.Workout.deleteMany({})
-  .then(() => db.Workout.collection.insertMany(workoutSeed))
+Workout.deleteMany({})
+  .then(() => Workout.collection.insertMany(workoutSeed))
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
